81e10c2ef90784a9eecc070a5b220490da1973aa
giginet <giginet.net@gmail.com>
2011-10-12 11:01:31 +0900

    設定を変更可能に

--- src/jp/ac/hokudai/virusim/gui/ControlPanel.java
+++ src/jp/ac/hokudai/virusim/gui/ControlPanel.java
@@ -15,6 +15,8 @@ import javax.swing.JPanel;
  *
  */
 public class ControlPanel extends JPanel{
+  private static final long serialVersionUID = 1L;
+
   public ControlPanel(){
     JButton testButton = new JButton("ぼたん");
     this.add(testButton);
